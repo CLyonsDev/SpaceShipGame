fcefd39afca278511d9b004ff45d43d5c919a0c0
Collin Lyons <brickmaster12@gmail.com>
2015-01-13 00:51:16 -0800

    Finally got lasers working!

---
+++ Assets/Scripts/MoveProjFwd.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveProjFwd : MonoBehaviour {
+	
+	public GameObject LaserGun;
+	//public Camera MainCamera=Camera.main;
+
+	// Use this for initialization
+	void Start () {
+		//this.transform.localRotation = LaserGun.transform.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Vector3 FwdForce = speed*transform.forward*Time.deltaTime;
+		//rigidbody.AddRelativeForce(0,0,speed*Time.deltaTime);	
+	}
+}
---
+++ Assets/shootonclick.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shootonclick : MonoBehaviour {
+
+	public Rigidbody laserBolt;
+
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			Rigidbody laserClone;
+			laserClone = Instantiate(laserBolt,transform.position,transform.rotation) as Rigidbody;
+			laserClone.velocity = transform.TransformDirection(Vector3.down * speed);
+		}
+		//Debug.Log("hue");
+		//laserBolt.rigidbody.velocity = transform.TransformDirection(new Vector3(0,0,speed*Time.deltaTime));
+		//laserClone.rigidbody.AddRelativeForce(0,0,speed*Time.deltaTime);
+	}
+}
