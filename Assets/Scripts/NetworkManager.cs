468712b5e4f9bf78ab91f3c07daadebd684fc2fd
Collin Lyons <brickmaster12@gmail.com>
2015-01-14 19:53:27 -0800

    Added basic automatic lobby joining functionality, and forced text serialization / visible meta files.

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	void Start()
+	{
+		Connect();
+	}
+
+	void Connect()
+	{
+		PhotonNetwork.ConnectUsingSettings("0.0.1");
+	}
+
+	void OnGUI(){
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+	void OnJoinedLobby(){
+		Debug.Log("Joined Lobby");
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	void OnPhotonRandomJoinFailed(){
+		PhotonNetwork.CreateRoom(null);
+	}
+
+	void OnJoinedRoom(){
+		Debug.Log("Joined a Room!");
+	}
+}
